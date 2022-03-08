2c2220f3f00404690a6c4ef57699fab7ad8731cd
Mohamed Ali <39552203+MohamedA-Ibrahim@users.noreply.github.com>
2022-03-09 00:16:40 +0200

    InitialCommit

---
+++ src/Ecommerce/Ecommerce.Repo/Repositories/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ecommerce.Repo.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T GetById(int Id);
+        Task<T> GetByIdAsync(int id);
+        void Add(T entity);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}

70b1f46bdd739ec0fa3fe4f2952a7872c718b329
Mohamed Ali <39552203+MohamedA-Ibrahim@users.noreply.github.com>
2022-03-31 16:45:09 +0200

    Implement Repository Pattern

---
+++ backend/src/Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public interface IRepository<T> where T : AuditableEntity
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ backend/src/Infrastructure/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Common;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : AuditableEntity
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet=_db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}

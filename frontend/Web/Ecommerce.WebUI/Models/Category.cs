1a0a9f902df360a8a54525b1b084d9ea6086d38f
Mohamed Ali <39552203+MohamedA-Ibrahim@users.noreply.github.com>
2022-05-17 23:53:09 +0200

    Fix docker leftovers that prevented publish

--- frontend/Web/Ecommerce.WebUI/Models/Category.cs
+++ frontend/Web/Ecommerce.WebUI/Models/Category.cs
@@ -1,9 +1,19 @@
-﻿namespace Ecommerce.WebUI.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.WebUI.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
 
+        [Required]
+        public string Description { get; set; }
+
+        [ValidateNever]
+        public string ImageUrl { get; set; }
     }
 }

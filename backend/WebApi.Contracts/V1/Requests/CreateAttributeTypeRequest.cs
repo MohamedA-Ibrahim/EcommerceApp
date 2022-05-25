a1e409e1f5679a0d1462e4619d8cf3cb5dcc2eec
Mohamed Ali <39552203+MohamedA-Ibrahim@users.noreply.github.com>
2022-05-25 22:27:21 +0200

    modify attributeTypeRequest

--- backend/WebApi.Contracts/V1/Requests/CreateAttributeTypeRequest.cs
+++ backend/WebApi.Contracts/V1/Requests/CreateAttributeTypeRequest.cs
@@ -3,6 +3,6 @@
     public class CreateAttributeTypeRequest
     {
         public int CategoryId { get; set; }
-        public string Name { get; set; }
+        public List<string> AttributeTypes { get; set; }
     }
 }
